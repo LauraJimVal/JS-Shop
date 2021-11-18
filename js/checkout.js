3b4385e6e676d31c0d98e17297d68dddeec0acb9
Laura Jiménez Valencia <laura_jimenez_valencia@hotmail.com>
2021-11-18 21:04:53 +0100

    Ejercicio 8 incompleto

--- js/checkout.js
+++ js/checkout.js
@@ -1,10 +1,10 @@
 // Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector(".phone");
-var names = document.querySelector(".names");
-var names2 = document.querySelector(".names2");
-var email = document.querySelector(".email"); // ubicamos el input con clase "email"
-var address= document.querySelector(".address"); // ubicamos el input con clase "address"
+var password = document.querySelector(".password").value;
+var phone = document.querySelector(".phone").value;
+var names = document.querySelector(".names").value;
+var names2 = document.querySelector(".names2").value;
+var email = document.querySelector(".email").value; 
+var address= document.querySelector(".address").value;
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
@@ -12,17 +12,36 @@ var errorName = document.getElementById('errorName');
 var errorPhone = document.getElementById('errorPhone');  
 
 var inputs = document.forms[0].elements; // guardamos los elementos del formulario en la variable inputs
+
 // Exercise 8
 function validate() {
-    var condicion_ok = true;
+
     for(var x = 0; x < inputs.length - 1; x++){ // Recorrido por los inputs del formulario
     // Condición para que todos los campos sean obligatorios
         if (inputs[x].value == "") { // Si el valor de cada input está vacío
         // if(names.value == "" || names2.value == "" || email.value == "" || password.value == "" || address.value == "" || phone.value == ""){ 
-            condicion_ok = false;
             alert("El campo " + inputs[x].name + " debe estar relleno."+"\n"+"Todos los campos son obligatorios"); // Mensaje de aviso de campos incompletos.
+            return false;
         }
     //condición para que los campos tengan almenos 3 carácteres
-        
+        if (inputs[x].value < 3) { // Si el valor de cada input tiene menos de 3 carácteres
+            alert("El campo " + inputs[x].name + " debe tener al menos 3 carácteres"); // Mensaje de aviso de carácteres insuficientes.
+            return false;
+        }
+
+    /*
+    // Validación de campos numéricos
+    var x = document.forms[0];
+    if(x.elements[0].matches(/^[0-9]+$/)){
+        errorName = document.getElementById('errorName').style.display = "block"; 
+        errorName = document.getElementById('errorName').style.color = "red";
+        return false;
     }
+    return true;
+    */
+    
+    
+    
+}
+    
 }
