e03dac92c2973846dea897aae2bc4246c56dd6a3
Laura Jiménez Valencia <laura_jimenez_valencia@hotmail.com>
2021-11-22 12:05:23 +0100

    ejercicio 8 primera condición

--- js/checkout.js
+++ js/checkout.js
@@ -10,6 +10,7 @@ var address= document.querySelector(".address").value;
 var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById('errorName');  
 var errorPhone = document.getElementById('errorPhone');  
+var mensajeError = document.getElementById("error"); // variable para guardar el mensaje de error
 
 var inputs = document.forms[0].elements; // guardamos los elementos del formulario en la variable inputs
 
@@ -19,15 +20,20 @@ function validate() {
     for(var x = 0; x < inputs.length - 1; x++){ // Recorrido por los inputs del formulario
     // Condición para que todos los campos sean obligatorios
         while(inputs[x].value == ""){ // Mientras los campos estén vacios
-            alert("El campo " + inputs[x].name + " debe estar relleno"); // Mensaje de alerta
+          //  alert("El campo " + inputs[x].name + " debe estar relleno. Todos los campos son ////obligatorios."); // Mensaje de alerta
             inputs[x].style.borderColor = "red"; // borde del imput rojo
-            return false; // todavía no se envia el formulario
+            mensajeError.innerHTML = "Rellena el campo " + inputs[x].name + "."; // mensaje de error para cada input
+            mensajeError.style.color = "red"; // color del mensaje de error
+            return false; // todavía no se envia el formulario por que no se cumplen las condiciones
         }
         if(inputs[x].value != ""){ // Si los campos estan rellenos
             inputs[x].style.borderColor = "black"; // borde del imput negro
+            //mensajeError.style.visibility = "hidden"; // mensaje de error se esconde
         }
+
+    // Condición para que todos los campos tengan almenos 3 carácteres
     }
-    return true;
+    return true; // si las condiciones se cumplen el formulario se envia
 }
     
 
